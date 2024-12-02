6b774f50ff99530f2f8bc2e7eae0844d4de7574c
ashanaroyals <kgashalatha3@gmail.com>
2024-12-02 15:46:27 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class string{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
