0427e7afd6032fd3170690fbe7e35189597b717e
ashanaroyals <kgashalatha3@gmail.com>
2024-12-02 15:18:18 +0530

    Add files via upload

---
+++ nextchar.java
@@ -0,0 +1,10 @@
+import java.util.*;
+    public class nextchar
+    {
+        public static void main(String args[]){
+        char c='a';
+        int b=c;
+        char v=(char)b;
+        System.out.println(v);
+            }
+}
